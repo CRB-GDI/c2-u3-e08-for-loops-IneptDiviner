a080ac4e60fff7fefcc7b8869c276832f62f7a71
IneptDiviner <145461276+IneptDiviner@users.noreply.github.com>
2024-02-28 10:19:32 -0500

    start

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Please enter a number to be the maximum");
+            
+            for (int n =int.Parse(Console.ReadLine()); ; )
+            {
+              
+            }
+        }
+    }
+}
